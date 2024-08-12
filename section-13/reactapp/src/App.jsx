206e825cbb6453acbd156787bd622840516c0b17
unknown <sarthakgupta1376@gmail.com>
2024-08-12 20:34:08 +0530

    optimized react

--- section-13/reactapp/src/App.jsx
+++ section-13/reactapp/src/App.jsx
@@ -3,31 +3,27 @@ import { useState } from 'react';
 import Counter from './components/Counter/Counter.jsx';
 import Header from './components/Header.jsx';
 import { log } from './log.js';
-
+import ConfigureCounter from './components/Counter/ConfigureCounter.jsx';
+// import 
 function App() {
   log('<App /> rendered');
 
-  const [enteredNumber, setEnteredNumber] = useState(0);
   const [chosenCount, setChosenCount] = useState(0);
 
-  function handleChange(event) {
-    setEnteredNumber(+event.target.value);
+  function handleSetCount(newCount){
+    // state update is scheduled and execute by react not done instantly and used in next line.
+    // setChosenCount(newCount);// this will triger a comp fn execution 
+    setChosenCount((prevChosenCount)=>prevChosenCount+1);
+    // React performs state batching,which simply means that multiple state updates that are triggered from the same function, for example,
+    //are batched together and will only lead to one component function execution.
   }
-
-  function handleSetClick() {
-    setChosenCount(enteredNumber);
-    setEnteredNumber(0);
-  }
-
   return (
     <>
       <Header />
       <main>
-        <section id="configure-counter">
-          <h2>Set Counter</h2>
-          <input type="number" onChange={handleChange} value={enteredNumber} />
-          <button onClick={handleSetClick}>Set</button>
-        </section>
+       <ConfigureCounter onSet={handleSetCount} />
+       {/* when chosenCount changes, react through away old component instance and recreate it newly   */}
+        <Counter key={chosenCount} initialCount={chosenCount} />
         <Counter initialCount={chosenCount} />
       </main>
     </>
--- section-13/reactapp/src/components/Counter/CounterHistory.jsx
+++ section-13/reactapp/src/components/Counter/CounterHistory.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Component, useState } from 'react';
 
 import { log } from '../../log.js';
 
@@ -23,9 +23,36 @@ export default function CounterHistory({ history }) {
 
   return (
     <ol>
-      {history.map((count, index) => (
+      {/* setting key to index of the count value here and that index is in the end not a value
+      that's strictly mapped to a specific count value. Instead, the index always stays the same.
+      The index of this item here will always be zero. The index of this item here will always be one.
+      But if I add a new value here to my list of changes, then of course those keys haven't changed.
+      But the values that belong to those keys haveand that's the problem with using index as a key value.
+      And that's why you typically shouldn't do thatand why you instead should try to use a key value
+      that is strictly connected to a specific value.
+      
+      Disadvantage-2 
+      Every time I click a button then all list items change.They all flash, which means they all change,
+      They all are touched by React. bcz React creates these DOM snapshots,it sees that in the end it now has a totally different
+      HTML code that should be rendered because a new item was added here.
+      And it therefore basically throws away the old list and re-renders the new list because everything changed.
+      And it's therefore able to reuse those old DOM elements
+      Using new key => instead of recreating them,it just inserts a new element in front of them.
+      And therefore, using such a key does not just help with state management, as shown in the previous lecture.
+      Instead, it also helps React render such lists  in a more optimal way.
+      */}
+      {/* {history.map((count, index) => (
         <HistoryItem key={index} count={count} />
+      ))} */}
+      {/* state here tied to a concrete instances */}
+      {history.map((count) => (
+        <HistoryItem key={count.id} count={count.value} />
       ))}
+
     </ol>
   );
 }
+//state belongs to concrete component instances, how can it jump across component instances? The position of this Component in the Component tree. React tracks state by component type(HistoryItem) & position(of that component)
+// in the tree.Because this is a problem that typically only occurs in lists like this because it can only occur
+//if you have sibling components that are of the same type  and the number or position of those components may change.
+// there you given wrong key
