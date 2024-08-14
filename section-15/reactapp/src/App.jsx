7590d794c0a1f0c622f10e6a9487e84c0aa9fdab
unknown <sarthakgupta1376@gmail.com>
2024-08-14 15:03:27 +0530

    completed http request

--- section-15/reactapp/src/App.jsx
+++ section-15/reactapp/src/App.jsx
@@ -6,12 +6,13 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
 import { updateUserPlaces } from './components/https.js';
+import Error from './components/Error.jsx';
 
 function App() {
   const selectedPlace = useRef();
 
   const [userPlaces, setUserPlaces] = useState([]);
-
+  const [errorUpdatingplace,setErrorUpdatingPlace] = useState();
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   function handleStartRemovePlace(place) {
@@ -22,9 +23,14 @@ function App() {
   function handleStopRemovePlace() {
     setModalIsOpen(false);
   }
-
+  // updating the data in the UI and sending the calling the api 
  async function handleSelectPlace(selectedPlace) {
+    
+    // alternative way-> first call api-> setLoader(true) if data comes set it to new data, else show an error UI
+    // await updateUserPlaces([selectedPlace,...userPlaces]);
+    // updating the data in the UI 
     setUserPlaces((prevPickedPlaces) => {
+      console.log(prevPickedPlaces);
       if (!prevPickedPlaces) {
         prevPickedPlaces = [];
       }
@@ -33,10 +39,16 @@ function App() {
       }
       return [selectedPlace, ...prevPickedPlaces];
     });
+    //  we can simply use our userPlaces state here because I have just updated the state in upper code,But as you learned before,
+      // the state update will not immediately be available in this next line of code.Instead, it will only be available
+      // after the component function executed the next time, which is scheduled by this state update here.
+      // calling the api 
     try{
       await updateUserPlaces([selectedPlace,...userPlaces]);
-    } catch (error){
-      console.log(error)
+    } catch (error){ // we have to manage 3 states
+      setUserPlaces(userPlaces);// if api not send it will again set to old UI
+      // This is optimal updating first set new UI, if api not send set UI to old one.
+      setErrorUpdatingPlace(error.message);
     }
   }
 
@@ -45,11 +57,33 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
     );
 
-    setModalIsOpen(false);
-  }, []);
+    try{
+    await updateUserPlaces( // add userPlaces in dependency array
+        userPlaces.filter((place)=> place.id!== selectedPlace.current.id)
+      )
+    } catch (error) {
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlace({
+        message:error.message || 'Failed to delete place'
+      })
+    }
 
+    setModalIsOpen(false);
+  }, [userPlaces]);
+
+  function handleError() {
+    setErrorUpdatingPlace(null);
+  }
   return (
     <>
+    <Modal open={errorUpdatingplace} onClose={handleError}>
+      {errorUpdatingplace &&(
+      <Error title="An error Occured"
+      message={errorUpdatingplace.message}
+      onConfirm={handleError}
+      />
+      )}
+    </Modal>
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
--- section-15/reactapp/src/components/https.js
+++ section-15/reactapp/src/components/https.js
@@ -1,10 +1,11 @@
-export async function updateUserPlaces(places) {// places(Array format) are not attachable, use  stringify
+
+export async function updateUserPlaces(places) {
    const response = await fetch('http://localhost:3000/user-places',
-        {   // configurational object
+        {   // configurational object(configure the request)
         method:'PUT',
-        body:JSON.stringify({places}),
-        headers:{
-            'Content-Type':'application/json'
+        body:JSON.stringify({places:places}), // places(Array format) are not attachable, use  stringify to convert it into json format
+        headers:{// metadata
+            'Content-Type':'application/json'  // data attached will be in json format
         }
     })
     const resData = await response.json();
