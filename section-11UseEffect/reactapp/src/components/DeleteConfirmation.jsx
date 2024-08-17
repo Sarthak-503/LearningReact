8106288bb48bc533cb92a08d5fb7985b3e7f23d1
unknown <sarthakgupta1376@gmail.com>
2024-08-17 13:11:07 +0530

    completed useEffect

--- section-11UseEffect/reactapp/src/components/DeleteConfirmation.jsx
+++ section-11UseEffect/reactapp/src/components/DeleteConfirmation.jsx
@@ -1,10 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import ProgressBar from "./progressBar";
 
+const TIMER = 3000;
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  console.log('Timer Set')
+ 
+
+  console.log("Timer Set");
   //But if I remove it myself before these three seconds expire,his timer will also still expire
-  // bcz we didn't stop it and that wasn't a problem here. it will be a problem if I click no bcz  
-  //you will see that even though I clicked no, after three seconds this item disappeared because 
+  // bcz we didn't stop it and that wasn't a problem here. it will be a problem if I click no bcz
+  //you will see that even though I clicked no, after three seconds this item disappeared because
   // the timer is never stopped when this component here is not rendered anymore.
   // Timer keeps on running whether the component is removed from dom
   // setTimeout(()=>{
@@ -12,17 +16,17 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
   //   onConfirm();
   // },3000)
 
-  useEffect(()=>{
-    const timer = setTimeout(()=>{
-    console.log("sasa");
-    onConfirm();
-  },3000)
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log("sasa");
+      onConfirm();
+    }, TIMER);
 
-  return () => {
-    console.log("Cleaning up the timer");
-    clearTimeout(timer)
-  }
-  },[onConfirm])
+    return () => {
+      console.log("Cleaning up the timer");
+      clearTimeout(timer);
+    };
+  }, [onConfirm]);
   // Here adding function can cause infinite loop, if App comp re-renders (where conConfirm value comes,
   // it will always be a different value irrespective of the same code(2 different fn haveing same code are not equal))
   // then this useEffect will be run again and if you click yes it will make a state change in App, App renders again
@@ -40,6 +44,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <ProgressBar timer={TIMER}   />
     </div>
   );
 }
---
+++ section-11UseEffect/reactapp/src/components/progressBar.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react'
+
+const ProgressBar = ({timer}) => {
+    const [remainingTime, setRemainingTime] = useState(timer);
+
+    useEffect(() => {
+      // execute after every 10 milli-seconds
+      const interval = setInterval(() => {
+        setRemainingTime((prevTime) => prevTime - 10);
+      }, 10);
+      return () =>{
+        clearInterval(interval)
+      }
+    }, []);
+  return (
+    <>
+       <progress value={remainingTime} max={timer}/>
+    </>
+  )
+}
+
+export default ProgressBar
