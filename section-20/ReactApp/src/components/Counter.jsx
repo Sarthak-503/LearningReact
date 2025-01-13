d2e8de1d28b835f45d50048cf9067eb21a427754
unknown <sarthak@enablero.com>
2025-01-13 16:19:01 +0530

    adding redux part

--- section-20/ReactApp/src/components/Counter.jsx
+++ section-20/ReactApp/src/components/Counter.jsx
@@ -1,15 +1,55 @@
-import classes from './Counter.module.css';
-
+import classes from "./Counter.module.css";
+import { useDispatch, useSelector } from "react-redux"; // select a part of state managed by store
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({
+      type: "toggle"
+    });
+  };
+  // this fn will be executed by react-redux
+  const counter = useSelector((state) => state.counter); // useSelector returns a part of state which i wanna extract
+  const showCounter = useSelector((state) => state.showCounter);
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({
+      type: "increment",
+      amount: 1,
+    });
+  };
+  const increaseHandler = () => {
+    dispatch({
+      type: "increase",
+      amount: 10,
+    });
+  };
+  const decrementHandler = () => {
+    dispatch({
+      type: "decrement",
+    });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      {showCounter && <div className={classes.value}>{counter}</div>}
+      <div className="counter">
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increment By 10 </button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}> Toggle Counter</button>
     </main>
   );
 };
 
 export default Counter;
+
+// useSelector
+// Now the great thing is that when you use useSelector,React Redux will automatically set up a subscription
+// to the Redux store for this component.So your component will be updated and will receive the latest counter
+//  automatically whenever that data changes in the Redux store.So it's an automatically reactive and changes to
+// the Redux store will cause this component function to be re executed.So you always have the latest counter.
+// That's why use selector is a very useful hook and why it is the hook we use for getting data out of the store.
+// If you ever would unmount this component (if it would be removed from the DOM for whatever reason),React Redux
+//  would also automatically clear the subscription for you.So it manages that subscription for you behind the scenes.
---
+++ section-20/ReactApp/src/store/index.js
@@ -0,0 +1,61 @@
+import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = { counter: 0, showCounter: true };
+
+// We are preparing a slice of our global state
+createSlice({
+
+})
+
+const counterReducer = (state = initialState, action) => {
+  if (action.type === "increment") {
+    // Point-1,So the objects which we return in the reducer will not, and that's super important
+    // will not be merged with the existing state.They will overwrite the existing state.
+
+    // state.counter++; //  Bad Practise
+    // Point-2 You should never super important never mutate the state, the existing state.
+    // You should never change the existing state. Instead, always override it by returning a brand new state object.
+    // And because objects and arrays are reference values in JavaScript, it's easy to accidentally override
+    // and change the existing state. that you do this in an immutable way
+
+    // that you'd never accidentally mutate your existing state
+    return {
+      counter: state.counter + 1,
+      showCounter: state.showCounter,
+    };
+  } else if (action.type === "increase") {
+    return {
+      counter: state.counter + action.amount, // reducer is dynamic
+      showCounter: state.showCounter,
+    };
+  } else if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  } else if (action.type === "toggle") {
+    return {
+      counter: state.counter,
+      showCounter: !state.showCounter,
+    };
+  }
+
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
+
+// Challenges
+// 1.The more data we have in the initialState, the more different pieces of state we have,
+// the bigger our state objects get.
+// And that means that we need to copy a lot of state when we update the counter we still need to copy
+// and keep all the other state properties, and it also means that this reducer function gets longer
+// and longer and all of a sudden we might have an unmaintainable big Redux file.
+// 2. State Immutibility
+// 3. There also are solutions for splitting your reducer into multiple smaller reducers
+// so that you don't get this large super big file, and there also our solutions and third-party packages
+// which allow you to automatically copy state and ensure that you don't accidentally edit it.
